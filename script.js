16514787f90cb1b4be902212a51ada44d470b442
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-14 17:10:05 -0400

    feature addColumn completed and uploaded

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ function addRow(){
     let table = document.getElementById("grid")
     let columns = table.rows[0].cells.length
     let row = table.insertRow()
-    for(let i = 0; i< columns; i++){
+    for(let i = 0; i < columns; i++){
         let cell = row.insertCell()
-        cell.innerHTML = "row"
+        cell.innerHTML = ""
     }
 }
+
+function addColumn(){
+    let table = document.getElementById("grid")
+    let rows = table.rows.length
+    //let row = table.insertRow()
+    for(let i = 0; i< rows; i++){
+        let row = table.rows[i]
+        let cell = row.insertCell()
+        cell.innerHTML = ""
+    }
+}
+
+
+
+
