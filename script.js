c276fe21b5858a1203490e241e452b82531cd10c
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-06-14 18:19:46 -0400

    single cell color change on lcik - feature added it

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-let table = document.getElementById("grid")
+let table = document.getElementById("grid");
 
 function addRow() {
-    let columns = table.rows[0].cells.length
-    let row = table.insertRow()
-    for (let i = 0; i < columns; i++) {
-        let cell = row.insertCell()
-        cell.innerHTML = ""
-    }
+  let columns = table.rows[0].cells.length;
+  let row = table.insertRow();
+  for (let i = 0; i < columns; i++) {
+    let cell = row.insertCell();
+    cell.innerHTML = "";
+  }
 }
 
 function addColumn() {
-    let rows = table.rows.length
+  let rows = table.rows.length;
+  for (let i = 0; i < rows; i++) {
+    let row = table.rows[i];
+    let cell = row.insertCell();
+    cell.innerHTML = "";
+  }
+}
+
+function removeRow() {
+  let rows = table.rows.length;
+  if (rows > 1) {
+    table.deleteRow(rows - 1);
+  }
+}
+
+function removeCol() {
+  let rows = table.rows.length;
+  if (rows > 0) {
+    let columns = table.rows[0].cells.length;
     for (let i = 0; i < rows; i++) {
-        let row = table.rows[i]
-        let cell = row.insertCell()
-        cell.innerHTML = ""
+      table.rows[i].deleteCell(columns - 1);
     }
-}
-
-
-function removeRow(){
-    let rows = table.rows.length
-    if (rows > 1) {
-        table.deleteRow(rows - 1)
-    }
-}
-
-function removeCol(){
-    let rows = table.rows.length
-    if(rows > 0){
-        let columns = table.rows[0].cells.length
-        for(let i = 0; i < rows; i++){
-            table.rows[i].deleteCell(columns - 1)
-        }
-    }
+  }
 }
 
 function selectColor() {
-    let dropdown = document.getElementById("dropdown");
-    // Public variable
-    newColor = dropdown.value;
+  let dropdown = document.getElementById("dropdown");
+  // Public variable
+  newColor = dropdown.value;
+
+  return newColor;
 }
 
+table.addEventListener("click", (event) => {
+  let cell = event.target;
 
-
-
+  if (cell.tagName === "TD") {
+    cell.style.backgroundColor = selectColor().toLowerCase();
+  }
+});
